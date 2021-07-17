0b5a00286991dca8329ad7b07df2f8cf7e436e1b
Abhijeet <77497897+abhijeet20h@users.noreply.github.com>
2021-07-17 21:51:52 +0530

    json data

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Service from "./components/Service";
 import Contact from "./components/Contact";
-import Details from './components/Details'
+import Details from "./components/Details";
+import Test from "./components/Test";
 import { Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [posts,setPosts]=useState([])
+useEffect(()=>{
+  const url='https://jsonplaceholder.typicode.com/posts';//api url
+  fetch(url).then(resp=>resp.json())//calling url by method GET
+  .then(resp=>setPosts(resp))//setting response to state posts
+},[])
+
+
   return (
     <div className="App">
       <>
         <Navbar />
+
+        <Test/>
+        {posts.map((post) => (
+        <div>
+          <p>{`${post.id}. ${post.title}`}</p>
+        </div>
+      ))}
         <Switch>
           <Route exact path="/">
             <Home />
---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+
+const Test = () => {
+  return (
+    <>
+      <h1 className="a">The TEST Page</h1>
+   
+    </>
+  );
+};
+export default Test;
